a943c26c3ef2a89405324965a4745539c3dab04e
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-11 14:33:28 -0600

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
-PLUGINSLUG=${PWD##*/} # returns basename of current directory
+PLUGINSLUG="force-plugin-updates-check" # returns basename of current directory
 CURRENTDIR=`pwd`
 MAINFILE="force-plugin-updates-check.php" # this should be the name of your main php file in the wordpress plugin
 
